9d631bce5f0670f0a4460805ca66a86eb2ac8ded
Coco <coulet.corentin@gmail.com>
2023-10-20 13:52:16 +0200

    correction des erreurs précédentes, ajout d'un distance et d'une map

--- main.js
+++ main.js
@@ -6,7 +6,20 @@ document.addEventListener("DOMContentLoaded", () => {
     const geolocateButton = document.getElementById("geolocateButton");
     let latitude;
     let longitude;
+    let map;
 
+    function calculateDistance(lat1, lon1, lat2, lon2) {
+        const R = 6371;
+        const dLat = (lat2 - lat1) * (Math.PI / 180);
+        const dLon = (lon2 - lon1) * (Math.PI / 180);
+        const a =
+            Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.cos(lat1 * (Math.PI / 180)) * Math.cos(lat2 * (Math.PI / 180)) *
+            Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        const distance = R * c;
+        return distance.toFixed(2);
+    }
 
     const getGeolocation = () => {
         if (navigator.geolocation) {
@@ -21,6 +34,7 @@ document.addEventListener("DOMContentLoaded", () => {
             addressDisplay.textContent = "La géolocalisation n'est pas prise en charge par votre navigateur.";
         }
     };
+
     const getAddressFromCoordinates = (longitude, latitude) => {
         const apiUrl = `https://api-adresse.data.gouv.fr/reverse/?lon=${longitude}&lat=${latitude}`;
         
@@ -34,29 +48,51 @@ document.addEventListener("DOMContentLoaded", () => {
                 console.error("Erreur lors de la récupération de l'adresse :", error);
             });
     };
+
     geolocateButton.addEventListener("click", getGeolocation);
     searchForm.addEventListener("submit", (e) => {
         e.preventDefault();
         const radius = 10;
         const coordinates = `POINT(${longitude}%20${latitude})`;
 
-    const cineUrl = `/api/explore/v2.1/catalog/datasets/etablissements-cinematographiques/records?where=within_distance(geolocalisation%2C%20geom'${coordinates}'%2C%20${radius}km)&limit=20`;
+        const cineUrl = `https://data.culture.gouv.fr/api/explore/v2.1/catalog/datasets/etablissements-cinematographiques/records?where=within_distance(geolocalisation%2C%20geom'${coordinates}'%2C%20${radius}km)&limit=20`;
 
-    fetch(cineUrl)
-    .then((response) => response.json())
-    .then((data) => {
-        cinemaList.innerHTML = "";
-        data.records.forEach((cinema) => {
-            const cinemaName = cinema.results.nom;
-            const cinemaAddress = cinema.results.adresse;
-            const cinemaCity = cinema.results.commune;
-            const cinemaElement = document.createElement("div");
-            cinemaElement.textContent = `Nom : ${cinemaName}, Adresse : ${cinemaAddress}, Ville : ${cinemaCity}`;
-            cinemaList.appendChild(cinemaElement);
-    });
-  })
-  .catch((error) => {
-    console.error("Erreur lors de la récupération des cinémas :", error);
-  });
+        fetch(cineUrl)
+        .then((response) => response.json())
+        .then((data) => {
+            cinemaList.innerHTML = "";
+            data.results.sort((cinemaA, cinemaB) => {
+                const distanceA = calculateDistance(latitude, longitude, cinemaA.latitude, cinemaA.longitude);
+                const distanceB = calculateDistance(latitude, longitude, cinemaB.latitude, cinemaB.longitude);
+                return distanceA - distanceB;
+            });
+            data.results.forEach((cinema) => {
+                const cinemaName = cinema.nom;
+                const cinemaAddress = cinema.adresse;
+                const cinemaCity = cinema.commune;
+                const distance = calculateDistance(latitude, longitude, cinema.latitude, cinema.longitude);
+                const cinemaElement = document.createElement("div");
+                cinemaElement.textContent = `Nom : ${cinemaName}, Adresse : ${cinemaAddress}, Ville : ${cinemaCity}, Distance : ${distance} km`;
+                cinemaList.appendChild(cinemaElement);
+            });
+
+            map = L.map('map').setView([latitude, longitude], 13);
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+            }).addTo(map);
+
+            data.results.forEach((cinema) => {
+                const cinemaName = cinema.nom;
+                const cinemaAddress = cinema.adresse;
+                const cinemaCity = cinema.commune;
+                const distance = calculateDistance(latitude, longitude, cinema.latitude, cinema.longitude);
+                L.marker([cinema.latitude, cinema.longitude])
+                    .addTo(map)
+                    .bindPopup(`Nom : ${cinemaName}<br>Adresse : ${cinemaAddress}<br>Ville : ${cinemaCity}<br>Distance : ${distance} km`);
+            });
+        })
+        .catch((error) => {
+            console.error("Erreur lors de la récupération des cinémas :", error);
+        });
     });
 });
